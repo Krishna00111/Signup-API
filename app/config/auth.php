cf4be2fa66da80f4afcbd69c0afcdc2e35dd28af
Chinmay Deshmukh <42055861+Krishna00111@users.noreply.github.com>
2021-06-19 17:28:07 +0530

    Create auth.php

---
+++ app/config/auth.php
@@ -0,0 +1,12 @@
+<?php
+return [
+'guards' => [ 
+        'web' => [ 
+            'driver' => 'session', 
+            'provider' => 'users', 
+        ], 
+        'api' => [ 
+            'driver' => 'passport', 
+            'provider' => 'users', 
+        ], 
+    ],
